75600b9911ebc1c501578de64af2c62edc87635e
SvetaNazarova <nazarovas84@gmail.com>
2023-02-25 22:25:58 +0200

    script fixed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let first_num = prompt("Enter the first number");
 let second_num = prompt("Enter the second number");
 
-alert((Number(first_num) + Number(second_num)) + ',' + "\n" + (first_num * second_num) + ',' + "\n" + (first_num - second_num) + ',' + "\n" + (second_num / first_num));
+alert((Number(first_num) + Number(second_num)) + ',' + "\n" + (first_num - second_num) + ',' + "\n" + (first_num * second_num) + ',' + "\n" + (second_num / first_num));
