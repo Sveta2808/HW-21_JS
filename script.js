3d6409a3596b5d8549fc906769c3c56a240887e4
SvetaNazarova <nazarovas84@gmail.com>
2023-03-02 14:37:54 +0200

    script added

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 let first_num = prompt("Enter the first number");
 let second_num = prompt("Enter the second number");
+let sum = Number(first_num) + Number(second_num)
+let difference = first_num - second_num
+let division = second_num / first_num
+let multiplication = first_num * second_num
+let warn_message = ("Are you sure you want to continue?")
 
-alert((Number(first_num) + Number(second_num)) + ',' + "\n" + (first_num - second_num) + ',' + "\n" + (first_num * second_num) + ',' + "\n" + (second_num / first_num));
+if (first_num === '' || second_num === '')
+    alert("Cannot enter");
+else {
+    alert(sum)
+    if (Number(first_num) < Number(second_num)) {
+        let confirmation = confirm(warn_message)
+        if (confirmation) {
+            alert(difference)
+            alert(multiplication)
+            if (Number(first_num) === 0) alert("error")
+            else {
+                alert(division)
+            }
+        }
+        else {
+            alert(sum)
+            alert(division)
+            alert(difference)
+            alert(multiplication)
+        }
+    }
+}
