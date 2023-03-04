94c4538121e4a3ca0c11bfb497607e88e4f7a2d5
SvetaNazarova <nazarovas84@gmail.com>
2023-03-04 21:02:54 +0200

    arraySum added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 let arr = [12,16,2,5,7,16,23,78,3,56]
+
+function arraySum(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+        }
+    console.log(sum);
+    }
+    arraySum(arr);
