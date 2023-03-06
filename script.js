6704d41fa84175743b5603046621e12b1eac620d
SvetaNazarova <nazarovas84@gmail.com>
2023-03-06 11:31:14 +0200

    triangle fixed

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let arr = [12, 16, 2, "tea", 7, 16, 23, "coffee", 3, 56];
 
-
-
-function arraySum(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
         if (isNaN(arr[i]) == true) { continue; }
@@ -11,9 +8,7 @@ function arraySum(arr) {
         }
     }
     alert(sum);
-}
 
-arraySum(arr);
 
 let min = null;
 let max = null;
@@ -28,14 +23,9 @@ for (let i = 0, len = arr.length; i < len; ++i) {
 alert("min = " + min + ", max = " + max);
 
 
-function makeLine(length) {
-    let line = '#';
-    for (let i = 1; i < length; i++) {
-        line += '#';
-    }
-    return line;
-}
 
-for (let i = 1; i <= 5; i++) {
-    console.log(makeLine(i));
+let line = '';
+for (let i = 0; i <= 5; i++) {
+    line += '#';
+    console.log(line);
 }
