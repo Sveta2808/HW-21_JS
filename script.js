45c7319f084e8a8215aae4e5a0fd1b93e1c5e9f4
SvetaNazarova <nazarovas84@gmail.com>
2023-03-06 11:04:12 +0200

    function fixed

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-let arr = [12, 16, 2, 5, 7, 16, 23, 78, 3, 56]
+let arr = [12, 16, 2, "tea", 7, 16, 23, "coffee", 3, 56];
+
+
 
 function arraySum(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
+        if (isNaN(arr[i]) == true) { continue; }
+        else {
+            sum += arr[i];
+        }
     }
     alert(sum);
 }
+
 arraySum(arr);
 
 let min = null;
 let max = null;
-for (let i = 0, len = arr.length; i < len; ++i)
-{
-    let elem = arr[i];
-    if (min === null || min > elem) min = elem;
-    if (max === null || max < elem) max = elem;
+for (let i = 0, len = arr.length; i < len; ++i) {
+    if (isNaN(arr[i]) == true) { continue; }
+    else {
+        let elem = arr[i];
+        if (min === null || min > elem) min = elem;
+        if (max === null || max < elem) max = elem;
+    }
 }
-alert( "min = " + min + ", max = " + max );
+alert("min = " + min + ", max = " + max);
 
 
 function makeLine(length) {
     let line = '#';
     for (let i = 1; i < length; i++) {
-        line += ' #';
+        line += '#';
     }
     return line;
 }
