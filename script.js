a25e82ef727d8bbcba35c7ba701302518a161cdf
SvetaNazarova <nazarovas84@gmail.com>
2023-03-28 12:48:41 +0300

    elements added on HTML

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const element = document.getElementById('item');
+
 let arr = [12, 16, 2, "tea", 7, 16, 23, "coffee", 3, 56];
 
 
@@ -9,7 +11,12 @@ function arraySum(arr) {
         else {
             sum += arr[i];
         }
+    
     }
+    const li = document.createElement('li');
+    li.textContent = sum;
+    element.appendChild(li);
+
     alert(sum);
 }
 
@@ -28,7 +35,17 @@ function arrayMax_Min(arr) {
             if (max === null || max < elem) max = elem;
         }
     }
+    const li_2 = document.createElement('li_2');
+    const li_3 = document.createElement('li_3');
+    li_2.textContent = min;
+    li_3.textContent = max;
+    element.appendChild(li_2);
+    element.appendChild(li_3);
+
     alert("min = " + min + ", max = " + max);
 }
 
 arrayMax_Min(arr);
+
+
+//element.innerHTML= '<li> item 1 </li>';
